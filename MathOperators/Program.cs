7c7718ad4ea07dcc3f25e93d6c02cd3ed0db1fb5
El Roble <san_lucas2005@yahoo.com>
2022-07-27 11:38:27 -0600

    Add project files.

---
+++ MathOperators/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int grade = 50;
+
+switch (grade)
+{
+    case int n when n <= 60:
+        Console.WriteLine("Failed");
+        break;
+    default:
+        Console.WriteLine("bla");
+        break;
+}
+
+var resultgrade =grade==50 ? "equals 50" : "different";
+
+void printName()
+{
+    Console.WriteLine("hi");
+}
+printName();
+
+
+
+
+
+
+
+
+
